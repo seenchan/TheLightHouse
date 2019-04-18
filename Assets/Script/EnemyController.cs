1730f022a2031acc9a2be89c2e0dd641f6fb8683
Chandra Sunarseto <chandra.sunarseto@live.com>
2019-04-18 10:24:19 +0700

    FIRST COMMIT

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = this.transform.position - Player.transform.position;
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(targetPos.x, 0, targetPos.z));
+    }
+}
